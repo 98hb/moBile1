981efb3b307e66065e11b32c8da719183a5a017b
‘98hb <w17339377595@163.com>
2022-02-27 14:02:03 +0800

    封装本地存储操作模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import { getItem, setItem } from '@/utils/storage'
 Vue.use(Vuex)
 
 const TOKEN_KEY = 'TOUTIAO_USER'
@@ -9,12 +9,14 @@ export default new Vuex.Store({
   state: {
     // user: null
     // user: window.localStorage.getItem(TOKEN_KEY)
-    user: JSON.parse(window.localStorage.getItem(TOKEN_KEY))
+    // user: JSON.parse(window.localStorage.getItem(TOKEN_KEY))
+    user: getItem(TOKEN_KEY)
   },
   mutations: {
     setUser (state, data) {
       state.user = data
-      window.localStorage.setItem(TOKEN_KEY, JSON.stringify(state.user))
+      // window.localStorage.setItem(TOKEN_KEY, JSON.stringify(state.user))
+      setItem(TOKEN_KEY, state.user)
     }
   },
   actions: {
