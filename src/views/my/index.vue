0370d39097236a9fa9e53213ca9e063960c07686
‘98hb <w17339377595@163.com>
2022-03-02 07:36:29 +0800

    画布个人中心

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -7,6 +7,40 @@
       <span class="text">登录 / 注册</span>
     </div>
   </div>
+    <div class="header user_info" >
+      <div class="base_info">
+        <div class="left">
+          <van-image
+            class="avatar"
+            src="https://img01.yzcdn.cn/vant/cat.jpeg"
+            round
+            fit="cover"
+          />
+          <span class="name">黑马头条号</span>
+        </div>
+        <div class="right">
+          <van-button size="mini" round="">编辑资料</van-button>
+        </div>
+      </div>
+      <div class="data_stats">
+        <div class="data_item">
+          <span class="count">10</span>
+          <span class="text">头条</span>
+        </div>
+        <div class="data_item">
+          <span class="count">10</span>
+          <span class="text">关注</span>
+        </div>
+        <div class="data_item">
+          <span class="count">10</span>
+          <span class="text">粉丝</span>
+        </div>
+        <div class="data_item">
+          <span class="count">10</span>
+          <span class="text">获赞</span>
+        </div>
+      </div>
+    </div>
 </div>
 </template>
 
@@ -79,6 +113,54 @@ export default {
         color: #fff;
       }
     }
+}
+
+.user_info {
+  .base_info {
+    height: 231px;
+    // background-color: red;
+    padding: 76px 32px 23px;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .left {
+      display: flex;
+      align-items: center;
+      .avatar {
+        width: 132px;
+        height: 132px;
+        margin-right: 23px;
+        border: 1px solid #fff;
+      }
+      .name {
+        font-size: 30px;
+        color: #fff;
+      }
+    }
+  }
+  .data_stats {
+    // height: 130px;
+    // background-color: #ccc;
+    display: flex;
+    .data_item {
+      height: 130px;
+      flex: 1;
+      display: flex;
+      justify-content: center; // jcc 水平居中
+      align-items: center; // aic 垂直居中
+      flex-direction: column; // 垂直分布 (默认水平分布)
+      color: #fff;
+      .count {
+        font-size: 36px;
+        // margin-bottom: 18px;
+      }
+      .text {
+        font-size: 23px;
+        // margin-bottom: 33px;
+      }
+    }
+  }
 }
 }
 </style>
