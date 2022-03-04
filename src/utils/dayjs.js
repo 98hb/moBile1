71f6682cf0e2ad4f54d1a7a102f744f10aefea4e
‘98hb <w17339377595@163.com>
2022-03-04 21:28:29 +0800

    初始化dayjs

---
+++ src/utils/dayjs.js
@@ -0,0 +1,9 @@
+import dayjs from 'dayjs'
+// 加载中文语言包
+import 'dayjs/locale/zh-cn'
+// dayjs 默认语言是英文,我们这里配置为中文
+// 全局使用
+dayjs.locale('zh-cn')
+
+// dayjs() 获取当前最新时间
+console.log(dayjs().format('YYYY-MM-DD'))
