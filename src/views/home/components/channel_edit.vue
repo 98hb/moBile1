f59fcbf8c36bd2741ffee9ee074bf8c00b70ffae
‘98hb <w17339377595@163.com>
2022-03-06 11:38:01 +0800

    展示推荐频道-获取所有频道

--- src/views/home/components/channel_edit.vue
+++ src/views/home/components/channel_edit.vue
@@ -53,6 +53,7 @@
 <script>
 // 这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 // 例如：import 《组件名称》 from '《组件路径》';
+import { getAllChannels } from '@/api/channel'
 
 export default {
 // import引入的组件需要注入到对象中才能使用
@@ -71,7 +72,7 @@ export default {
   data () {
     // 这里存放数据
     return {
-
+      allChannels: [] // 所有频道
     }
   },
   // 监听属性 类似于data概念
@@ -80,11 +81,19 @@ export default {
   watch: {},
   // 方法集合
   methods: {
-
+    async loadAllChannels () {
+      try {
+        const { data } = await getAllChannels()
+        // console.log(data)
+        this.allChannels = data.data.channels
+      } catch (err) {
+        this.$toast('数据获取失败')
+      }
+    }
   },
   // 生命周期 - 创建完成（可以访问当前this实例）
   created () {
-
+    this.loadAllChannels()
   },
   // 生命周期 - 挂载完成（可以访问DOM元素）
   mounted () {
