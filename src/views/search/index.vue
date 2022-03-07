80480b54e7e12481bebbed48ed11e87c7f689a74
‘98hb <w17339377595@163.com>
2022-03-07 10:16:38 +0800

    搜索栏

---
+++ src/views/search/index.vue
@@ -0,0 +1,72 @@
+<!--  -->
+<template>
+<div class='search_container'>
+    <!-- 顶部搜索栏 -->
+      <form action="/">
+        <van-search
+          v-model="searchText"
+          show-action
+          placeholder="请输入搜索关键词"
+          @search="onSearch"
+          @cancel="onCancel"
+          background="#3296fa"
+        />
+      </form>
+    <!-- /顶部搜索栏 -->
+</div>
+</template>
+
+<script>
+// 这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+// 例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+// import引入的组件需要注入到对象中才能使用
+  name: 'SearchIndex',
+  components: {},
+  props: {},
+  data () {
+    // 这里存放数据
+    return {
+      searchText: ''
+    }
+  },
+  // 监听属性 类似于data概念
+  computed: {},
+  // 监控data中的数据变化
+  watch: {},
+  // 方法集合
+  methods: {
+    onSearch (val) {
+      console.log(val)
+    },
+    onCancel () {
+      // console.log('onCancel')
+      this.$router.back()
+    }
+  },
+  // 生命周期 - 创建完成（可以访问当前this实例）
+  created () {
+
+  },
+  // 生命周期 - 挂载完成（可以访问DOM元素）
+  mounted () {
+
+  },
+  beforeCreate () {}, // 生命周期 - 创建之前
+  beforeMount () {}, // 生命周期 - 挂载之前
+  beforeUpdate () {}, // 生命周期 - 更新之前
+  updated () {}, // 生命周期 - 更新之后
+  beforeDestroy () {}, // 生命周期 - 销毁之前
+  destroyed () {}, // 生命周期 - 销毁完成
+  activated () {} // 如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script>
+<style lang='less' scoped>
+//@import url(); 引入公共css类
+.search_container {
+  .van-search__action {
+    color: #fff;
+  }
+}
+</style>
